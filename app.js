466e193ec5ad497c00bc540a960525b01382f3f2
AyaanMansoor <154600399+AyaanMansoor@users.noreply.github.com>
2024-03-02 17:37:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+// Q1
+// let username;
+
+
+
+//Q2
+// let myName="ayaanMansoor"
+// console.log(myName)
+
+
+
+//Q3
+//let message="Hello World"
+//alert(message)
+
+
+
+//Q4
+// let username="Ayaan Mansoor"
+// let ag="16 years old"
+// let profession="Certified Mobile Application Development"
+// alert(username);
+// alert(ag);
+// alert(profession);
+
+
+//Q5
+//let pizza = "pizza\npizz\npiz\npi\np";
+//alert(pizza);
+
+
+
+
+//Q6
+// var email = "ayaanmansoor00@gmail.com.com";
+// alert("My email address is " + email);
+
+
+//Q7
+//let book="A smarter way to learn JavaScript."
+//alert("I am trying to learn from the book " + book)
+
+//Q8
+//document.write("Yah! I can write HTML content through JavaScript")
+
+
+
+//Q9
+ //let design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//alert(design)
+
+
+
+
